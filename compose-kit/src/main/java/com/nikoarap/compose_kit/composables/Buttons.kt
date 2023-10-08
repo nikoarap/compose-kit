b25685703dc28720e1b61e06631ebfc597956428
nikoarap <nikoarap@gmail.com>
2023-10-08 09:40:53 +0300

    updated Buttons.kt

--- compose-kit/src/main/java/com/nikoarap/compose_kit/composables/Buttons.kt
+++ compose-kit/src/main/java/com/nikoarap/compose_kit/composables/Buttons.kt
@@ -5,11 +5,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.sp
 
 /**
  * Wrapper to the Button composable.
@@ -104,6 +107,114 @@ fun ButtonWithTextColumn(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Button(
+            modifier = modifier,
+            onClick = onClick,
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = backgroundColor,
+                contentColor = contentColor
+            )
+        ) {
+            Text(buttonText)
+        }
+    }
+}
+
+/**
+ * Wrapper to the OutlinedButton composable.
+ * Composes an outlined button with a text inside and an onClick listener. Button and text color can be styled.
+ *
+ *
+ * @param modifier              Modifier to apply attributes to. You can specify the width and height of the button in the modifier you pass to this function
+ * @param buttonText            text to be displayed on the button
+ * @param backgroundColor       background button color
+ * @param contentColor          content button color
+ * @param onClick               onClick listener
+ *
+ */
+@Composable
+fun OutlinedButtonWithText(
+    modifier: Modifier,
+    buttonText: String,
+    backgroundColor: Color,
+    contentColor: Color,
+    onClick: () -> Unit
+) {
+    OutlinedButton(
+        modifier = modifier,
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = backgroundColor,
+            contentColor = contentColor
+        )
+    ) {
+        Text(buttonText)
+    }
+}
+
+/**
+ * Wrapper to the OutlinedButton composable.
+ * Composes a Row containing an outlined button with a text inside and an onClick listener. Button and text color can be styled.
+ *
+ *
+ * @param modifier              Modifier to apply attributes to. You can specify the width and height of the button in the modifier you pass to this function
+ * @param buttonText            text to be displayed on the button
+ * @param backgroundColor       background button color
+ * @param contentColor          content button color
+ * @param onClick               onClick listener
+ *
+ */
+@Composable
+fun OutlinedButtonWithTextRow(
+    modifier: Modifier,
+    buttonText: String,
+    backgroundColor: Color,
+    contentColor: Color,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        OutlinedButton(
+            modifier = modifier,
+            onClick = onClick,
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = backgroundColor,
+                contentColor = contentColor
+            )
+        ) {
+            Text(buttonText)
+        }
+    }
+}
+
+/**
+ * Wrapper to the OutlinedButton composable.
+ * Composes a Column containing an outlined button with a text inside and an onClick listener. Button and text color can be styled.
+ *
+ *
+ * @param modifier              Modifier to apply attributes to. You can specify the width and height of the button in the modifier you pass to this function
+ * @param buttonText            text to be displayed on the button
+ * @param backgroundColor       background button color
+ * @param contentColor          content button color
+ * @param onClick               onClick listener
+ *
+ */
+@Composable
+fun OutlinedButtonWithTextColumn(
+    modifier: Modifier,
+    buttonText: String,
+    backgroundColor: Color,
+    contentColor: Color,
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedButton(
             modifier = modifier,
             onClick = onClick,
             colors = ButtonDefaults.buttonColors(
