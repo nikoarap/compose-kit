c89777efa8338b37b509991b199a482eb36a6cd2
nikoarap <nikoarap@gmail.com>
2023-10-18 21:34:59 +0300

    updated Charts.kt

--- compose-kit/src/main/java/com/nikoarap/compose_kit/composables/Charts.kt
+++ compose-kit/src/main/java/com/nikoarap/compose_kit/composables/Charts.kt
@@ -5,13 +5,20 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -29,6 +36,7 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 import com.nikoarap.compose_kit.models.PieChartSegment
 import com.nikoarap.compose_kit.utils.Constants.Companion.DP_16
+import com.nikoarap.compose_kit.utils.Constants.Companion.DP_24
 import com.nikoarap.compose_kit.utils.Constants.Companion.DP_32
 import com.nikoarap.compose_kit.utils.Constants.Companion.DP_48
 import com.nikoarap.compose_kit.utils.Constants.Companion.DP_8
@@ -48,37 +56,40 @@ import com.nikoarap.compose_kit.utils.Constants.Companion.ZERO_FLOAT
  *
  *
  * @param segmentItems              A list of [PieChartSegment] objects representing the segments of the pie chart.
+ * @param backGroundColor           The background color of the chart and content.
+ * @param topPaddingDp              The top padding of the chart in dp
+ * @param bottomPaddingDp           The bottom padding of the content in dp
  * @param chartOuterRadius          The outer radius of the pie chart.
  * @param chartBarWidth             The width of the chart bars.
- * @param animDuration              The duration of the chart animation.
+ * @param animDurationMs            The duration of the chart animation in ms.
  */
 @Composable
 fun DetailedPieChart(
     segmentItems: List<PieChartSegment>,
+    backGroundColor: Color,
+    topPaddingDp: Int,
+    bottomPaddingDp: Int,
     chartOuterRadius: Int,
     chartBarWidth: Int,
-    animDuration: Int,
+    animDurationMs: Int,
 ) {
     val totalSum: Int = segmentItems.sumOf { it.value }
     var animationPlayed by remember { mutableStateOf(false) }
     var lastValue = ZERO_FLOAT
 
-    //diameter value of the Pie
     val animateSize by animateFloatAsState(
         targetValue = if (animationPlayed) chartOuterRadius * TWO_FLOAT else ZERO_FLOAT,
         animationSpec = tween(
-            durationMillis = animDuration,
+            durationMillis = animDurationMs,
             delayMillis = ZERO,
             easing = LinearOutSlowInEasing
         ), label = EMPTY
     )
 
-    // if you want to stabilize the Pie Chart you can use value -90f
-    // 90f is used to complete 1/4 of the rotation
     val animateRotation by animateFloatAsState(
         targetValue = if (animationPlayed) NINETY_FLOAT * ELEVEN_FLOAT else ZERO_FLOAT,
         animationSpec = tween(
-            durationMillis = animDuration,
+            durationMillis = animDurationMs,
             delayMillis = ZERO,
             easing = LinearOutSlowInEasing
         ), label = EMPTY
@@ -88,9 +99,15 @@ fun DetailedPieChart(
         animationPlayed = true
     }
     Column(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .verticalScroll(rememberScrollState())
+            .background(backGroundColor),
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(modifier = Modifier.height(DP_24.dp))
+        Spacer(modifier = Modifier.height(topPaddingDp.dp))
         Box(
             modifier = Modifier.size(animateSize.dp),
             contentAlignment = Alignment.Center
@@ -114,6 +131,7 @@ fun DetailedPieChart(
             }
         }
         DetailsPieChart(segmentItems)
+        Spacer(modifier = Modifier.height(bottomPaddingDp.dp))
     }
 }
 
@@ -127,7 +145,7 @@ fun DetailedPieChart(
 private fun DetailsPieChart(segmentItems: List<PieChartSegment>) {
     Column(
         modifier = Modifier
-            .padding(top = EIGHTY.dp)
+            .padding(top = DP_48.dp)
             .fillMaxWidth()
     ) {
         for (segmentItem in segmentItems) {
