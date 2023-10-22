d5d71aa57b660f4324dd85ae9e0a9bcd7cbf7fb0
nikoarap <nikoarap@gmail.com>
2023-10-22 13:59:26 +0300

    updated TabItem.kt

--- compose-kit/src/main/java/com/nikoarap/compose_kit/models/TabItem.kt
+++ compose-kit/src/main/java/com/nikoarap/compose_kit/models/TabItem.kt
@@ -2,6 +2,13 @@ package com.nikoarap.compose_kit.models
 
 import com.nikoarap.compose_kit.utils.Constants
 
+/**
+ * Data class representing an item in a tab row.
+ *
+ * @property order          The order in which this tab item should appear.
+ * @property label          The label or text associated with the tab item.
+ * @property onClick        The lambda to be invoked when this tab item is clicked.
+ */
 data class TabItem(
     var order: Int = 0,
     var label: String = Constants.EMPTY,
