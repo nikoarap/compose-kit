8661d6a38139c6df8e4d72ac2ade585d9333ca3e
nikoarap <nikoarap@gmail.com>
2023-11-06 19:15:28 +0200

    removed unused imports

--- app/src/main/java/com/nikoarap/composekitlibrary/MainActivity.kt
+++ app/src/main/java/com/nikoarap/composekitlibrary/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.nikoarap.composekitlibrary
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,11 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.nikoarap.compose_kit.composables.CheckboxWithText
-import com.nikoarap.compose_kit.composables.DetailedPieChart
-import com.nikoarap.compose_kit.composables.SwitchButtonWithIcon
-import com.nikoarap.compose_kit.models.PieChartSegment
 import com.nikoarap.composekitlibrary.ui.theme.ComposeKitLibraryTheme
 
 class MainActivity : ComponentActivity() {
@@ -45,22 +39,6 @@ class MainActivity : ComponentActivity() {
                 .fillMaxSize()
                 .background(Color.White),
         ) {
-
-            val segmentItems = listOf(
-                PieChartSegment(label = "Water", value = 70, color = Color(0xFF6495ED), upperTextTypography = MaterialTheme.typography.bodyMedium, MaterialTheme.typography.bodySmall, Color.Black, Color.LightGray),
-                PieChartSegment(label = "Malt", value = 8, color = Color.Yellow, upperTextTypography = MaterialTheme.typography.bodyMedium, MaterialTheme.typography.bodySmall, Color.Black, Color.LightGray),
-                PieChartSegment(label = "Hops", value = 4, color = Color.Green, upperTextTypography = MaterialTheme.typography.bodyMedium, MaterialTheme.typography.bodySmall, Color.Black, Color.LightGray),
-                PieChartSegment(label = "Yeast", value = 18, color = Color.Gray, upperTextTypography = MaterialTheme.typography.bodyMedium, MaterialTheme.typography.bodySmall, Color.Black, Color.LightGray))
-
-                DetailedPieChart(
-                    segmentItems = segmentItems,
-                    backgroundColor = Color.White,
-                    cardPaddingDp = 8.dp,
-                    cardElevationDp = 8.dp,
-                    chartOuterRadius = 80,
-                    chartBarWidth = 80,
-                    animDurationMs = 1600
-                )
 
         }
     }
